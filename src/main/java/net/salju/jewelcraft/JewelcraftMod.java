7e13c6ee622d16cc4966d0cb42360a6db3388449
Salju <61861078+Jusey1@users.noreply.github.com>
2023-07-29 01:58:21 -0400

    Add files via upload

---
+++ src/main/java/net/salju/jewelcraft/JewelcraftMod.java
@@ -0,0 +1,23 @@
+package net.salju.jewelcraft;
+
+import net.salju.jewelcraft.events.*;
+import net.salju.jewelcraft.init.*;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.common.MinecraftForge;
+
+@Mod("jewelcraft")
+public class JewelcraftMod {
+	public static final String MODID = "jewelcraft";
+
+	public JewelcraftMod() {
+		MinecraftForge.EVENT_BUS.register(this);
+		MinecraftForge.EVENT_BUS.register(new JewelcraftLoot());
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		JewelryItems.REGISTRY.register(bus);
+		JewelryEnchantments.REGISTRY.register(bus);
+		JewelryTabs.REGISTRY.register(bus);
+	}
+}
